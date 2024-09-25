f09d459ad665bb07dabe25ba245fce6f620f1b2f
marcelo <marceloviana1991@gmail.com>
2024-09-25 16:16:06 -0300

    inserindo jogador

---
+++ src/Jogador.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Jogador {
+    private String nome;
+    private int[] jogo = new int[2];
+    private double premiado = 0;
+
+    public Jogador(String nome, int primeiroNumero, int segundoNumero) {
+        this.nome = nome;
+        this.jogo[0] = primeiroNumero;
+        this.jogo[1] = segundoNumero;
+    }
+
+    public int[] getJogo() {
+        return jogo;
+    }
+
+    public void inserirPremiacao() {
+        this.premiado = premiado + 0.5;
+    }
+
+    @Override
+    public String toString() {
+        return "Jogador{" +
+                "nome='" + nome + '\'' +
+                ", jogo=" + Arrays.toString(jogo) +
+                ", premiado=" + premiado +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         int numenos[] = new int[10];
 
+        Jogador jogador = new Jogador("Marcelo", 10, 26);
+
         for(int i = 0; i < numenos.length; i++) {
             numenos[i] = (int)Math.round(Math.random() * 100);
             for(int j = 0; j < i; j++) {
@@ -14,7 +16,18 @@ public class Main {
             }
         }
 
+        for (int i = 0; i < jogador.getJogo().length; i++) {
+            for(int j = 0; j < numenos.length; j++) {
+                if (jogador.getJogo()[i] == numenos[j]) {
+                    jogador.inserirPremiacao();
+                }
+            }
+        }
+
+
+
         Arrays.sort(numenos);
         System.out.println(Arrays.toString(numenos));
+        System.out.println(jogador);
     }
 }
